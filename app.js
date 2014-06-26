14e8ea57dac54e13f8d16ccf5cbf1d5e48c08d36
dave12345 <dothedu22@yahoo.com>
2014-06-26 13:34:26 -0700

    Node.js second assignment

---
+++ app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./library/mathlib');
+
+console.log('Sum = ', mathlib.sum(3,5));
+console.log('Multiply = ', mathlib.multiply(3,5));
+console.log('Square = ', mathlib.square(5));
+console.log('Random = ', mathlib.random(10,15));
